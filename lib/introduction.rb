5f97287b1cf96f4ea6890bd416902b9380dcc274
Rose Jean Louis <r.d.jeanlouis01@gmail.com>
2021-08-11 22:15:55 -0400

    Completed assignment

---
+++ lib/introduction.rb
@@ -0,0 +1,11 @@
+#The #introduction Method
+def introduction (name)
+    puts "Hi, my name is #{name}."
+end
+introduction("Josh")
+
+#The #introduction_with_language Method
+def introduction_with_language(name, language)
+    puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Peter", "C#")
